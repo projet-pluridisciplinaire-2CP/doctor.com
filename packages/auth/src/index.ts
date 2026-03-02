69494afced0037379c03e4a7a32cd575c777b026
Khaled Walid Amara <ok_amara@esi.dz>
2026-03-02 16:42:52 +0100

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,32 @@
+import { expo } from "@better-auth/expo";
+import { db } from "@doctor.com/db";
+import * as schema from "@doctor.com/db/schema/auth";
+import { env } from "@doctor.com/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [
+    env.CORS_ORIGIN,
+    "doctor.com://",
+    ...(env.NODE_ENV === "development"
+      ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+      : []),
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [expo()],
+});
